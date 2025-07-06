edaa5b9f068005298f1d88455815147113e4b728
Afif Fakhri <piipps.f@gmail.com>
2025-07-07 00:09:17 +0700

    All Fixed

--- Assets/Scripts/BacksoundManager.cs
+++ Assets/Scripts/BacksoundManager.cs
@@ -1,19 +1,35 @@
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class BacksoundManager : MonoBehaviour
 {
     private static BacksoundManager instance;
+    private AudioSource audioSource;
+
+    [Header("Bisa Diisi dari Inspector")]
+    public AudioClip backsoundClip;
 
     void Awake()
     {
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject); // Jangan hancurkan saat ganti scene
+            DontDestroyOnLoad(gameObject);
+
+            // Siapkan AudioSource
+            audioSource = GetComponent<AudioSource>();
+            audioSource.loop = true;
+            audioSource.playOnAwake = false;
+
+            if (backsoundClip != null)
+            {
+                audioSource.clip = backsoundClip;
+                audioSource.Play(); // volume akan ikut media volume handphone
+            }
         }
         else
         {
-            Destroy(gameObject); // Hancurkan duplikat jika sudah ada satu
+            Destroy(gameObject);
         }
     }
 }
